1db8f8562f6139619f17ed2a7d56d0f124b425e4
DESKTOP-V8PO33B\likeit_student <tk.rlyuj.0042@gmail.com>
2018-08-20 17:32:15 +0900

    10-3

--- Practice/src/practice10/PTra10_02.java
+++ Practice/src/practice10/PTra10_02.java
@@ -16,24 +16,24 @@ public class PTra10_02 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
-
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
-
 		// ★ 変数usに格納されているインスタンスのuserIdフィールドに、101を代入してください
-
-
 		// ★ 変数usに格納されているインスタンスのuserNmフィールドに、"リゾーム太郎"を代入してください
-
-
 		// ★ 変数usに格納されているインスタンスのmailフィールドに、"rhizome@hoge.com"を代入してください
-
-
 		// ★ 変数usに格納されているインスタンスのpasswordフィールドに、"1qazxcv"を代入してください
-
-
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
 
+		User us = new User();
+		us.userId=101;
+		us.userNm="リゾーム太郎";
+		us.mail="rhizome@hoge.com";
+		us.password="1qazxcv";
+		System.out.println(us.userId);
+		System.out.println(us.userNm);
+		System.out.println(us.mail);
+		System.out.println(us.password);
+		
+		
+		
 	}
 }
